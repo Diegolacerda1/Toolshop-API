4f93afbefad686a8efc677d592a0e789bc90ecf3
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-06-12 16:01:23 -0400

    api register new user and login created

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,41 @@
-describe('API authentication - Toolshop website', () => {
-  it('Should authenticate user and get token', () => {
+describe('API Testing with Cypress - Toolshop website', () => {
+  it('Should create a new user', () => {
     // cy.visit('https://api.practicesoftwaretesting.com')
-    cy.visit('https://practicesoftwaretesting.com/#/')
+    // cy.request('POST', 'https://api.practicesoftwaretesting.com/users/register', {
+    //     "first_name": "Johny",
+    //     "last_name": "Doe",
+    //     "address": "Street 1",
+    //     "city": "City",
+    //     "state": "State",
+    //     "country": "Country",
+    //     "postcode": "1234AA",
+    //     "phone": "0987654321",
+    //     "dob": "1970-01-01",
+    //     "password": "Supersecret1@",
+    //     "email": "john@doe.example"
+    // }).then((response) => {
+    //   expect(response.status).to.equal(201);
+    // })
+
+    cy.request({
+      method: 'POST',
+      url: 'https://api.practicesoftwaretesting.com/users/login',
+      body: {
+        email: 'customer@practicesoftwaretesting.com',
+        password: 'welcome01'
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.property('access_token');
+    
+      const token = response.body.access_token;
+      cy.log(token);
+    });
+    
+
   })
+  
 })
