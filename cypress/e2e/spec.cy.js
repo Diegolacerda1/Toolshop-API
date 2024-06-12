2cdbca03f94295d4f6315b9540e2a65854d54055
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-06-12 15:30:53 -0400

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,6 @@
+describe('API authentication - Toolshop website', () => {
+  it('Should authenticate user and get token', () => {
+    // cy.visit('https://api.practicesoftwaretesting.com')
+    cy.visit('https://practicesoftwaretesting.com/#/')
+  })
+})
